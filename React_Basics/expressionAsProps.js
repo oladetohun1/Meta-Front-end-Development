f9101e1514606e8d59a46926595c7c3fb6398f12
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-03-02 14:57:17 +0100

    multiple component

---
+++ React_Basics/expressionAsProps.js
@@ -0,0 +1,26 @@
+const bool = false;
+const str1 = "just";
+
+function Example(props) {
+    return (
+        <div>
+            <h2>
+                The value of the toggleBoolean prop is:{props.toggleBoolean.toString()}
+            </h2>
+            <p>The value of the math prop is: <em>{props.math}</em></p>
+            <p>The value of the str prop is: <em>{props.str}</em></p>
+        </div>
+    );
+};
+
+export default function App() {
+    return (
+        <div className="App">
+            <Example
+                toggleBoolean={!bool}
+                math={(10 + 20) / 3}
+                str={str1 + ' another string'}
+            />
+        </div>
+    );
+};
