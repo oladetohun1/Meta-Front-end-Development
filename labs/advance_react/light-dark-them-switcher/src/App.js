a06a15673080212cc4062f96902a5e3904463bde
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-05-27 13:09:56 +0100

    toggle theme switcher

---
+++ labs/advance_react/light-dark-them-switcher/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import index from "../src/Switch/index";
+
+function App() {
+  return (
+    <div className="App">
+      <index/>
+    </div>
+  );
+}
+
+export default App;
