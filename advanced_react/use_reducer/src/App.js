ce51a2d7571ecfc28e601c57b73770e607bb64d3
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-06-04 22:55:56 +0100

    useing useReducer

--- advanced_react/use_reducer/src/App.js
+++ advanced_react/use_reducer/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react";
 
+const reducer = (state, action) => {
+  if (action.type === 'buy_ingredients') return {money:state.money - 10};
+    if (action.type === "sell_ingredients") return { money: state.money + 10 };
+  if (action.type === "celebrity_visit") return { money: state.money + 5000 };
+  return state;
+}
 function App() {
+  const initialState = {money:100};
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wallet: {state.money}</h1>
+      <div>
+        <button onClick={() => dispatch({ type: "but_ingredients" })}>
+          shopping for veggies!
+        </button>
+        <button onClick={() => dispatch({ type: "sell_ingredients" })}>
+          serve a meal to the customer
+        </button>
+        <button onClick={() => dispatch({ type: "celebrity_visit" })}>
+          celebrity_visit
+        </button>
+      </div>
     </div>
   );
 }
