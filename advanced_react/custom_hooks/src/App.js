9331ba022c428a0b0d90df3be3236f1e798f4566
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-06-05 23:51:47 +0100

    custom hooks

---
+++ advanced_react/custom_hooks/src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import useConsoleLog from "./useConsoleLog";
+function App() {
+  const [count, setCount] = useState(0);
+  useConsoleLog(count);
+  
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>Plus 1</button>
+    </div>
+  );
+}
+
+export default App;
